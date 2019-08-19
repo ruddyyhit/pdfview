ec7adb35e7f535c7dcc2979cfafcbcf070529ec6
ruddyyhit <47591711+ruddyyhit@users.noreply.github.com>
2019-08-19 10:14:16 +0530

    Update LoginController.java

--- src/main/java/com/example/pdfview/controller/LoginController.java
+++ src/main/java/com/example/pdfview/controller/LoginController.java
@@ -3,7 +3,7 @@ package com.example.pdfview.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.example.pdfview.service.LoginService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 @SessionAttributes("name")
+@RequestMapping("/")
 public class LoginController {
 	
 	@Autowired
 	LoginService service;
+	
+	
 	@RequestMapping(value="/login",
 			method =RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
